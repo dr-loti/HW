d2fa2db421da44b6c54a22d5321cffaf564beca8
Loti Drobnic <loti.dr@gmail.com>
2020-04-17 23:24:48 +0200

    mood_fix2

--- HW/python/mood_check.py
+++ HW/python/mood_check.py
@@ -1,5 +1,5 @@
 
-mood = input("Hi, how do you feel or in what mood are you today?: ").capitalize().lower()
+mood = input("Hi, how do you feel or in what mood are you today?: ").capitalize()
 
 if mood == "happy":
     print("It is great to see you happy!")
