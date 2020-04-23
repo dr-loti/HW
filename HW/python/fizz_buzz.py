547ed30002b2e795c0dfbe12c20129301c88ee51
Loti Drobnic <loti.dr@gmail.com>
2020-04-23 02:29:36 +0200

    fizz_buzz_add

---
+++ HW/python/fizz_buzz.py
@@ -0,0 +1,18 @@
+
+print('Hi, lets play a game, caled FizzBuzz.')
+
+num = int(input("Select a number between 1 and 100: "))
+
+for x in range (1, num+1):
+
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+
+    elif x % 3 == 0:
+        print("fizz")
+
+    elif x % 5 == 0:
+        print("buzz")
+
+    else:
+        print(x)
