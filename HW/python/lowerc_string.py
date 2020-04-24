4ca54befb219cb3119d30e960e23ab12c5242e9d
Loti Drobnic <loti.dr@gmail.com>
2020-04-24 20:08:09 +0200

    lowerc_string

--- HW/python/lowerc_string.py
+++ HW/python/lowerc_string.py
@@ -4,3 +4,10 @@ print(string.lower())
 
 string = "Today Is A BeautiFul DAY"
 print(string.lower())
+
+print("")
+
+input = input("Enter an uppercase sentence you want to transform into lowercase: ")
+string = input.lower()
+
+print(string)
